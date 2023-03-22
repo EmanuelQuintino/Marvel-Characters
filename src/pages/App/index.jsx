f8cf0654654705b21fda870826bfb726e8aed0a7
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-22 01:00:52 -0300

    add banner marvel

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -24,15 +24,23 @@ export function App() {
   console.log(characters[10]);
   return (
     <Container>
+      {/* <img src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2F4.bp.blogspot.com%2F-DtrEvc5Hbz4%2FV0biXpR4wRI%2FAAAAAAAABSU%2F19vhjcYJMFwDsnuLfxr_E2bQOT26CUaVwCLcB%2Fs1600%2Fbanner-logo-marvel-2.jpg&sp=1679456989T2c2c51ebc53cb73d1edf4b32a36decf7c229ff11f61b69de416f8e8731b1263c" alt="" className="logoMarvel"/> */}
+      
+      <img src="https://m.media-amazon.com/images/S/abs-image-upload-na/6/AmazonStores/A1F83G8C2ARO7P/0742df6fb0bfa5eaf657483f504df0e2.w3000.h600.jpg" alt="" className="logoMarvel"/>
+
       <h1>Home</h1>
       <div className="characterCards">
         { characters && 
           characters.map((character, index) => {
             return (
               <div className="characterCard" key={index}>
-                <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt=""/>
-                <p>{character.name}</p>
-                <p>About</p>
+                <div className="part1">
+                  <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt=""/>
+                </div>
+                <div className="part2">
+                  <h3>{character.name}</h3>
+                  <a href="#">About</a>
+                </div>
               </div>
             )
           })
--- src/pages/App/style.js
+++ src/pages/App/style.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.section `
+  .logoMarvel {
+    width: 100%;
+  }
+  
   h1 {
     font-size: 1.6rem;
   }
@@ -10,11 +14,52 @@ export const Container = styled.section `
     align-items: center;
     justify-content: center;
     flex-wrap: wrap;
+    gap: 2.4rem;
+  }
+
+  .characterCard {
+    width: 24.0rem;
+    height: 30.0rem;
+    border-radius: .4rem;
+    box-shadow: .01rem .01rem .2rem .1rem black;
+
+    transition: 300ms;
+  }
+
+  .characterCard:hover {
+    scale: 1.05;
+  }
+
+  .characterCard .part1 {
+    overflow: hidden;
+    width: 24.0rem;
+    height: 24.0rem;
+    border-radius: .4rem .4rem 0 0;
   }
 
   .characterCard img {
-    width: 3.2rem;
+    width: 100%;
+    height: 100%;
+    
+    object-fit: cover;
+    object-position: center;
+
     transition: brightness 300ms;
     box-shadow: .01rem .01rem .2rem .1rem black;
+  }
+
+  .part2 {
+    display: grid;
+    place-content: center;
+  }
+
+  h3 {
+    font-size: 1.6rem;
+    margin: 1.0rem 0 0;
+  }
+
+  a {
+    text-align: center;
+    color: red;
   }
 `; 
