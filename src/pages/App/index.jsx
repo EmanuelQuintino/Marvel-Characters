9a019a61d1bc18c6b54ed2b46be181095f73aba6
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-21 22:28:16 -0300

    add dependencies

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Container } from "./style"
+import axios from "axios"
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState({});
+
+  const API = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8e7fb05ab6b828a15d90074ae8106e06&hash=203f8e9960ae6528a06db554ddafcbff"
+
+  useEffect(() => {
+    axios.get(API)
+    .then((res) => setCharacters(res.data))
+    .catch((error) => console.error(error));
+  }, [])
+
+  console.log(characters);
 
   return (
     <Container>
